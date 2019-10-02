401dd9f6aadedd29e8ab516273b45a331fc78413
strakerak <smmt@live.com>
2019-10-02 00:02:46 -0500

    Added enemy and follow script

---
+++ IsoJack/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float health;
+    private Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) < 5)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, target.position) < 1)
+            health = health - 10;
+        //if (health <= 0)
+            //Destroy(gameObject);
+    }
+}
