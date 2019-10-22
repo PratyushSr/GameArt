62860d23ca765db88925620bf458a1dab0c84a3c
David Seijas <ds168100@outlook.com>
2019-10-22 18:38:06 -0500

    Fixed the dialogue view so it could be triggered via script (and added pausing so the character moves)

--- IsoJack/Assets/Scripts/DialougeView.cs
+++ IsoJack/Assets/Scripts/DialougeView.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class DialougeView : MonoBehaviour
 {
+    public static DialougeView converstationInstance = null;
     public Button triggerChoices;
     public Image npcPhoto;
     public Image playerPhoto;
@@ -12,12 +13,16 @@ public class DialougeView : MonoBehaviour
     public Text npcLabel;
     public Text dialouge;
 
-    public Canvas choices;
+    //public Canvas choices;
+    public GameObject choiceButtons;
+    public GameObject overlay;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (converstationInstance == null) converstationInstance = this;
+        else if (converstationInstance != this) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -28,20 +33,24 @@ public class DialougeView : MonoBehaviour
 
     public void showDialougeChoices()
     {
-        Vector3 playerPhotoPos = playerPhoto.transform.position;
-        Vector3 npcPhotoPos = npcPhoto.transform.position;
-        Vector3 boxPos = dialougeBox.transform.position;
-        Vector3 namePos = npcLabel.transform.position;
-        Vector3 dialougePos = dialouge.transform.position;
+        if(choiceButtons.activeInHierarchy == false)
+        {
+            choiceButtons.SetActive(true);
+            Vector3 playerPhotoPos = playerPhoto.transform.position;
+            Vector3 npcPhotoPos = npcPhoto.transform.position;
+            Vector3 boxPos = dialougeBox.transform.position;
+            Vector3 namePos = npcLabel.transform.position;
+            Vector3 dialougePos = dialouge.transform.position;
 
-        playerPhoto.transform.position = new Vector3(playerPhotoPos.x, playerPhotoPos.y + 200, playerPhotoPos.z);
-        npcPhoto.transform.position = new Vector3(npcPhotoPos.x, npcPhotoPos.y + 200, npcPhotoPos.z);
-        dialougeBox.transform.position = new Vector3(boxPos.x, boxPos.y + 200, boxPos.z);
-        npcLabel.transform.position = new Vector3(namePos.x, namePos.y + 200, namePos.z);
-        dialouge.transform.position = new Vector3(dialougePos.x, dialougePos.y + 200, dialougePos.z);
-        
-        npcLabel.text = "Tavern Maid";
+            playerPhoto.transform.position = new Vector3(playerPhotoPos.x, playerPhotoPos.y + 200, playerPhotoPos.z);
+            npcPhoto.transform.position = new Vector3(npcPhotoPos.x, npcPhotoPos.y + 200, npcPhotoPos.z);
+            dialougeBox.transform.position = new Vector3(boxPos.x, boxPos.y + 200, boxPos.z);
+            npcLabel.transform.position = new Vector3(namePos.x, namePos.y + 200, namePos.z);
+            dialouge.transform.position = new Vector3(dialougePos.x, dialougePos.y + 200, dialougePos.z);
+            dialouge.text = "Please select a dialogue choice.";
+        }
     }
+        
 
     public void moveBack()
     {
@@ -56,6 +65,7 @@ public class DialougeView : MonoBehaviour
         dialougeBox.transform.position = new Vector3(boxPos.x, boxPos.y - 200, boxPos.z);
         npcLabel.transform.position = new Vector3(namePos.x, namePos.y - 200, namePos.z);
         dialouge.transform.position = new Vector3(dialougePos.x, dialougePos.y - 200, dialougePos.z);
+        choiceButtons.SetActive(false);
     }
 
     public void choicePressed(string choiceNum)
