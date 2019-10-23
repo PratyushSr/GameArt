3b94b469a83ca80fca5bad9d340555d6b9a90262
Unknown <Aaron1997ps@gmail.com>
2019-10-22 19:06:34 -0500

    Fixed some Merge Problems.

--- IsoJack/Assets/Scripts/DialougeView.cs
+++ IsoJack/Assets/Scripts/DialougeView.cs
@@ -14,8 +14,7 @@ public class DialougeView : MonoBehaviour
     public Text dialouge;
 
     //public Canvas choices;
-    public GameObject choiceButtons;
-    public GameObject overlay;
+    //public GameObject overlay;
 
     // Start is called before the first frame update
     void Start()
@@ -33,22 +32,22 @@ public class DialougeView : MonoBehaviour
 
     public void showDialougeChoices()
     {
-        if(choiceButtons.activeInHierarchy == false)
-        {
-            choiceButtons.SetActive(true);
-            Vector3 playerPhotoPos = playerPhoto.transform.position;
-            Vector3 npcPhotoPos = npcPhoto.transform.position;
-            Vector3 boxPos = dialougeBox.transform.position;
-            Vector3 namePos = npcLabel.transform.position;
-            Vector3 dialougePos = dialouge.transform.position;
+        //if(choiceButtons.activeInHierarchy == false)
+        //{
+        //choiceButtons.SetActive(true);
+        Vector3 playerPhotoPos = playerPhoto.transform.position;
+        Vector3 npcPhotoPos = npcPhoto.transform.position;
+        Vector3 boxPos = dialougeBox.transform.position;
+        Vector3 namePos = npcLabel.transform.position;
+        Vector3 dialougePos = dialouge.transform.position;
 
-            playerPhoto.transform.position = new Vector3(playerPhotoPos.x, playerPhotoPos.y + 200, playerPhotoPos.z);
-            npcPhoto.transform.position = new Vector3(npcPhotoPos.x, npcPhotoPos.y + 200, npcPhotoPos.z);
-            dialougeBox.transform.position = new Vector3(boxPos.x, boxPos.y + 200, boxPos.z);
-            npcLabel.transform.position = new Vector3(namePos.x, namePos.y + 200, namePos.z);
-            dialouge.transform.position = new Vector3(dialougePos.x, dialougePos.y + 200, dialougePos.z);
-            dialouge.text = "Please select a dialogue choice.";
-        }
+        playerPhoto.transform.position = new Vector3(playerPhotoPos.x, playerPhotoPos.y + 200, playerPhotoPos.z);
+        npcPhoto.transform.position = new Vector3(npcPhotoPos.x, npcPhotoPos.y + 200, npcPhotoPos.z);
+        dialougeBox.transform.position = new Vector3(boxPos.x, boxPos.y + 200, boxPos.z);
+        npcLabel.transform.position = new Vector3(namePos.x, namePos.y + 200, namePos.z);
+        dialouge.transform.position = new Vector3(dialougePos.x, dialougePos.y + 200, dialougePos.z);
+        //dialouge.text = "Please select a dialogue choice.";
+        //}
     }
         
 
@@ -65,12 +64,12 @@ public class DialougeView : MonoBehaviour
         dialougeBox.transform.position = new Vector3(boxPos.x, boxPos.y - 200, boxPos.z);
         npcLabel.transform.position = new Vector3(namePos.x, namePos.y - 200, namePos.z);
         dialouge.transform.position = new Vector3(dialougePos.x, dialougePos.y - 200, dialougePos.z);
-        choiceButtons.SetActive(false);
+        //choiceButtons.SetActive(false);
     }
 
     public void choicePressed(string choiceNum)
     {
-        dialouge.text = "You pressed dialogue choice " + choiceNum + ".";
+        //dialouge.text = "You pressed dialogue choice " + choiceNum + ".";
         moveBack();
     }
 }
