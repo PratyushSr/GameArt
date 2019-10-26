fbda104e75d818d12e9694d0d0d866fa98f04604
Unknown <Aaron1997ps@gmail.com>
2019-10-25 21:23:20 -0500

    [A] Added Player and NPC Sprites to Dialgue Tree
    Aaron-
    + Added NPC Sprite and Player Sprite to DialogueTree
       * Leaving as None will not change the sprite
    + Optomized the loading of the DIalogue Tree so that it loads the same frame instead of one frame after.

--- IsoJack/Assets/Scripts/SetConversationTree.cs
+++ IsoJack/Assets/Scripts/SetConversationTree.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetConversationTree : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class SetConversationTree : MonoBehaviour
      * 1 = 4 - way Choice
      * 2 = Exit
      */
+    public List<Sprite> NPCSprite;
+    public List<Sprite> PlayerSprite;
     public List<string> NPCName;
     public List<string> dialogueText;
     public List<string> ChoiceWarps; //Only for Dialogue Options Warp1;Warp2;Warp3;Warp4
@@ -17,14 +20,14 @@ public class SetConversationTree : MonoBehaviour
     private int tp; //Text Position
     private GameObject ConversationView;
     private GameObject DialogueTextObject;
+    private GameObject NPCPortrait;
+    private GameObject PlayerPortrait;
     private GameObject NPCNameObject;
     private GameObject ChoicesCanvas;
     private GameObject Dia1;
     private GameObject Dia2;
     private GameObject Dia3;
     private GameObject Dia4;
-
-    private bool loadDialogue;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +38,8 @@ public class SetConversationTree : MonoBehaviour
     {
         ConversationView = GameObject.Find("ConversationView");
         DialogueTextObject = GameObject.Find("ConversationView/DialogueText");
+        NPCPortrait = GameObject.Find("ConversationView/npcPortrait");
+        PlayerPortrait = GameObject.Find("ConversationView/playerPortrait");
         NPCNameObject = GameObject.Find("ConversationView/NPCNameTag");
         ChoicesCanvas = GameObject.Find("ConversationView/choicesCanvas");
         Dia1 = GameObject.Find("ConversationView/choicesCanvas/DialogueOptionOne");
@@ -43,7 +48,7 @@ public class SetConversationTree : MonoBehaviour
         Dia4 = GameObject.Find("ConversationView/choicesCanvas/DialogueOptionFour");
         Debug.Log("Started Conversation");
         tp = textPosition;
-        loadDialogue = true;
+        loadDialogue();
     }
 
     // Update is called once per frame
@@ -53,75 +58,80 @@ public class SetConversationTree : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && dialogueType[tp] == 0)
         {
             tp += 1;
-            loadDialogue = true;
+            loadDialogue();
         }
         
-        if (loadDialogue)
-        {
-            Debug.Log(tp);
-            loadDialogue = false;      
-            if (tp >= dialogueText.Count || dialogueType[tp] == 2)
-            {
-                Debug.Log("Ended Conversation");
-                gameObject.GetComponent<SetConversationTree>().enabled = false;
-                ChoicesCanvas.SetActive(true);
-                Dia1.SetActive(true);
-                Dia2.SetActive(true);
-                Dia3.SetActive(true);
-                Dia4.SetActive(true);
-                ConversationView.SetActive(false);
-            }
-            else
-            {
-          
-                if (dialogueType[tp] == 0)
-                {
-                    ChoicesCanvas.SetActive(false);
-                    DialogueTextObject.GetComponent<UnityEngine.UI.Text>().text = dialogueText[tp];
-                }
-                else if (dialogueType[tp] == 1)
-                {
-                    DialogueTextObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 0);
-                    ConversationView.GetComponent<DialougeView>().showDialougeChoices();
-                    ChoicesCanvas.SetActive(true);
-                    int c = CountSections(dialogueText[tp]);
-                    Dia1.SetActive(false);
-                    Dia2.SetActive(false);
-                    Dia3.SetActive(false);
-                    Dia4.SetActive(false);
-                    
-                    if (c > 1)
-                    {
-                        Dia1.SetActive(true);
-                        Dia1.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 1);
-                    }
-                    if (c > 2)
-                    {
-                        Dia2.SetActive(true);
-                        Dia2.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 2);
-                    }
-                    if (c > 3)
-                    {
-                        Dia3.SetActive(true);
-                        Dia3.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 3);
-                    }
-                    if (c > 4)
-                    {
-                        Dia4.SetActive(true);
-                        Dia4.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 4);
-                    }
-                }
-                if (NPCName[tp] != "") NPCNameObject.GetComponent<UnityEngine.UI.Text>().text = NPCName[tp];
-            }
-        }
         if (dialogueType[tp] == 1)
         {
             int choice = ConversationView.GetComponent<DialougeView>().getChoicePressed();
             if (choice != 0)
             {
                 tp = int.Parse(GetSection(ChoiceWarps[tp], choice - 1));
-                loadDialogue = true;
+                loadDialogue();
             }
+        }
+    }
+
+    void loadDialogue()
+    {
+        Debug.Log(tp);
+        if (tp >= dialogueText.Count || dialogueType[tp] == 2)
+        {
+            Debug.Log("Ended Conversation");
+            gameObject.GetComponent<SetConversationTree>().enabled = false;
+            ChoicesCanvas.SetActive(true);
+            Dia1.SetActive(true);
+            Dia2.SetActive(true);
+            Dia3.SetActive(true);
+            Dia4.SetActive(true);
+            ConversationView.SetActive(false);
+        }
+        else
+        {
+
+            if (dialogueType[tp] == 0)
+            {
+                ChoicesCanvas.SetActive(false);
+                DialogueTextObject.GetComponent<UnityEngine.UI.Text>().text = dialogueText[tp];
+            }
+            else if (dialogueType[tp] == 1)
+            {
+                DialogueTextObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 0);
+                ConversationView.GetComponent<DialougeView>().showDialougeChoices();
+                ChoicesCanvas.SetActive(true);
+                int c = CountSections(dialogueText[tp]);
+                Dia1.SetActive(false);
+                Dia2.SetActive(false);
+                Dia3.SetActive(false);
+                Dia4.SetActive(false);
+
+                if (c > 1)
+                {
+                    Dia1.SetActive(true);
+                    Dia1.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 1);
+                }
+                if (c > 2)
+                {
+                    Dia2.SetActive(true);
+                    Dia2.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 2);
+                }
+                if (c > 3)
+                {
+                    Dia3.SetActive(true);
+                    Dia3.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 3);
+                }
+                if (c > 4)
+                {
+                    Dia4.SetActive(true);
+                    Dia4.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Text>().text = GetSection(dialogueText[tp], 4);
+                }
+            }
+            //Update Name
+            if (NPCName[tp] != "") NPCNameObject.GetComponent<UnityEngine.UI.Text>().text = NPCName[tp];
+            //Update NPC Sprite
+            if (NPCSprite[tp] != null) NPCPortrait.GetComponent<UnityEngine.UI.Image>().sprite = NPCSprite[tp];
+            //Update Player Sprite
+            if (PlayerSprite[tp] != null) PlayerPortrait.GetComponent<UnityEngine.UI.Image>().sprite = PlayerSprite[tp];
         }
     }
 
