f8fb11ef720a7c98c57fa1b8d90381edc4d732a9
Aquila <aglxiv@gmail.com>
2019-11-05 10:13:09 -0600

    Revert "Merge branch 'ART_BRANCH' into DEV_BRANCH"
    This reverts commit 88c4358138a186f12a9770941769e6016d6d9c6a.

--- IsoJack/Assets/Scripts/Enemy.cs
+++ IsoJack/Assets/Scripts/Enemy.cs
@@ -4,83 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float health;
-
-    public bool aggressive = false;
-
-    public float moveSpeed;
-    public float stopDistance;
-    private Transform target;
-    public CharTalk talk;
-
-
-    //combat stuff
-    private float attackCd = 0;
-    public float attackTimer;
-    public Transform attackPos;
-    public LayerMask whatIsEnemies;
-    public float attackRange;
-    public float damage;
-
+    public int health;
     
 
-    void Start()
-    {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
 
     void Update()
     {
-        if (aggressive == true)
-        {
-
-            if (Vector2.Distance(transform.position, target.position) < 5 && Vector2.Distance(transform.position, target.position) > 1)
-                transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-
-            if (attackCd <= 0)
-            {
-
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                    enemiesToDamage[i].GetComponent<PlayerAttack>().TakeDamage(damage);
-                }
-
-                attackCd = attackTimer;
-
-            }
-
-            else
-            {
-                attackCd -= Time.deltaTime;
-            }
-        }
-
         if (health <= 0)
         {
             Destroy(gameObject);
 
         }
-    }
 
-    
-
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
 
     }
 
-   
-
-    public void EnemyTakeDamage(float damage)
+    public void TakeDamage(int damage)
     {
 
         health -= damage;
-        Debug.Log("Enemy takes damage!!!");
+        Debug.Log("damage taken!!!");
 
-        aggressive = true;
     }
 }
