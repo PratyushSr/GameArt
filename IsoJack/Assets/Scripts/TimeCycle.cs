6169a9aeb08bad7ebefe46ce2eb1e68ecf4e2bda
David Seijas <ds168100@outlook.com>
2019-11-22 23:04:02 -0600

    Fixed Day/Night Indicator
    The day night indicator will now properly update whenever the time of day changes

--- IsoJack/Assets/Scripts/TimeCycle.cs
+++ IsoJack/Assets/Scripts/TimeCycle.cs
@@ -7,9 +7,6 @@ public class TimeCycle : MonoBehaviour
     // Start is called before the first frame update
 
     public float timer = 720;
-    public GameObject icon;
-    public GameObject nightcon;
-    public GameObject duskcon;
     public float INTIMERDONTCHANGE;
     void Start()
     {
@@ -19,23 +16,27 @@ public class TimeCycle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(INTIMERDONTCHANGE.ToString());
         if(INTIMERDONTCHANGE>=480&&INTIMERDONTCHANGE<=720)
         {
-            icon.SetActive(true);
-            duskcon.SetActive(false);
-            nightcon.SetActive(false);
+            //icon.SetActive(true);
+            //duskcon.SetActive(false);
+            //nightcon.SetActive(false);
+            GameManager.instance.timeIndicator.sprite = GameManager.instance.timeOfDay[0];
         }
         else if(INTIMERDONTCHANGE>=360&&INTIMERDONTCHANGE<=480)
         {
-            icon.SetActive(false);
-            duskcon.SetActive(true);
-            nightcon.SetActive(false);
+            //icon.SetActive(false);
+            //duskcon.SetActive(true);
+            //nightcon.SetActive(false);
+            GameManager.instance.timeIndicator.sprite = GameManager.instance.timeOfDay[1];
         }
         else
         {
-            icon.SetActive(false);
-            duskcon.SetActive(false);
-            nightcon.SetActive(true);
+            //icon.SetActive(false);
+            //duskcon.SetActive(false);
+            //nightcon.SetActive(true);
+            GameManager.instance.timeIndicator.sprite = GameManager.instance.timeOfDay[3];
         }
 
         if(INTIMERDONTCHANGE>=0)
