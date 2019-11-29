165022a0e16bb79b4e567941d6d5e1b3adb51c2d
Unknown <Aaron1997ps@gmail.com>
2019-11-28 20:57:57 -0600

    Upgraded translucentWhenBehind to have more options

--- IsoJack/Assets/Scripts/TraslucentWhenBehind.cs
+++ IsoJack/Assets/Scripts/TraslucentWhenBehind.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TraslucentWhenBehind : MonoBehaviour
 {
     public GameObject ObjectToTrack;
+    public bool CircleCollision;
+    public double radius;
+    public double xoffset;
+    public double yoffset;
+    public double xAdditionalRadiusIfRectangle;
+    public double yAdditionalRadiusIfRectangle;
+
+    //Leaving everything blank keeps it at default 10 width and 5 height above the object.
+    //-------------------
 
     private bool isTranslucent;
     private SpriteRenderer image;
@@ -14,15 +24,30 @@ public class TraslucentWhenBehind : MonoBehaviour
     void Start()
     {
         isTranslucent = false;
+        if (radius == 0)
+        {
+            radius = 2.5;
+            xAdditionalRadiusIfRectangle = 2.5;
+            yoffset = 2.5;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool playerIsBehind = ((transform.position.y < ObjectToTrack.transform.position.y)
-            && (transform.position.y+5 > ObjectToTrack.transform.position.y)
-            && (transform.position.x-5 < ObjectToTrack.transform.position.x)
-            && (transform.position.x+5 > ObjectToTrack.transform.position.x));
+        bool playerIsBehind;
+        if (CircleCollision)
+        {
+            //Distance Formula
+            playerIsBehind = Math.Sqrt(Math.Pow(transform.position.x+xoffset- ObjectToTrack.transform.position.x, 2) + 
+                Math.Pow(transform.position.y + yoffset - ObjectToTrack.transform.position.y,2)) <= radius;
+        }
+        else {
+            playerIsBehind = ((transform.position.y - radius - yAdditionalRadiusIfRectangle + yoffset< ObjectToTrack.transform.position.y)
+                && (transform.position.y + radius + yAdditionalRadiusIfRectangle + yoffset > ObjectToTrack.transform.position.y)
+                && (transform.position.x - radius - xAdditionalRadiusIfRectangle + xoffset < ObjectToTrack.transform.position.x)
+                && (transform.position.x + radius + xAdditionalRadiusIfRectangle + xoffset > ObjectToTrack.transform.position.x));
+        }
         //Debug.Log(playerIsBehind);
         if (isTranslucent && !playerIsBehind)
         {
