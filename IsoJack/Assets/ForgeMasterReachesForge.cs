5835ed7a0092a54363a7f3b542da66698d3e0e0f
Aaron Laptop <Aaron1997ps@gmail.com>
2019-12-07 17:53:37 -0600

    Added ForgeMasterReachesForge

---
+++ IsoJack/Assets/ForgeMasterReachesForge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ForgeMasterReachesForge : MonoBehaviour
+{
+    private GameObject forgeBuilding;
+    private bool reached;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        forgeBuilding = GameObject.Find("IsoJack_Overworld/Buildings/Forge");
+        reached = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!reached && Vector2.Distance(transform.position, forgeBuilding.transform.position) < 5)
+        {
+            reached = true;
+            GetComponent<EnemyFollow>().enabled = false;
+            transform.position = new Vector3(-21.7f, 8.7f, 0);
+        }
+    }
+}
