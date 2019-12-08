aa5e7f9d67759f277272c8dc4219f11fce3ca684
Aaron Laptop <Aaron1997ps@gmail.com>
2019-12-07 18:38:59 -0600

    Forge Master Quest Completely Done Now

--- IsoJack/Assets/ForgeMasterReachesForge.cs
+++ IsoJack/Assets/ForgeMasterReachesForge.cs
@@ -7,22 +7,40 @@ public class ForgeMasterReachesForge : MonoBehaviour
 {
     private GameObject forgeBuilding;
     private bool reached;
+    private bool questActive;
     
     // Start is called before the first frame update
     void Start()
     {
         forgeBuilding = GameObject.Find("IsoJack_Overworld/Buildings/Forge");
         reached = false;
+        questActive = false;
+        transform.position += transform.right * 1000;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!reached && Vector2.Distance(transform.position, forgeBuilding.transform.position) < 5)
+        if (questActive)
         {
-            reached = true;
-            GetComponent<EnemyFollow>().enabled = false;
-            transform.position = new Vector3(-21.7f, 8.7f, 0);
+            if (!reached && Vector2.Distance(transform.position, forgeBuilding.transform.position) < 5)
+            {
+                reached = true;
+                GetComponent<EnemyFollow>().enabled = false;
+                transform.position = new Vector3(-21.7f, 8.7f, 0);
+                while(Adventureog.advLogInstance.Quest[3].subQuest < 3)
+                {
+                    Adventureog.advLogInstance.addProgress(4, 1);
+                }
+            }
+        }
+        else
+        {
+            if (Adventureog.advLogInstance.Quest[3].subQuest == 1)
+            {
+                questActive = true;
+                transform.position += transform.right * -1000;
+            }
         }
     }
 }
