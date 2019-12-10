623d9865d4c0b96a4cb824eb893f3da43122c938
Aaron Laptop <Aaron1997ps@gmail.com>
2019-12-09 23:08:25 -0600

    Woops, found more stuff to upload

--- IsoJack/Assets/DoggoFollow.cs
+++ IsoJack/Assets/DoggoFollow.cs
@@ -6,15 +6,21 @@ public class DoggoFollow : MonoBehaviour
 {
     [Tooltip("You better put the doggos owner here! Doggo Demands it!")]
     public GameObject GiveMeMyOwner;
+    [Tooltip("I'm a good boi! This is when I stand still and await your return")]
+    public Sprite IStand;
+    [Tooltip("This is when I walk to you, my loyal friend")]
+    public Sprite IWalk;
 
     private bool whereMaLooking;
     private bool dogToy____OHBOYOHBOYOHBOY;
+    private bool meAnimationIsRunnin;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<EnemyFollow>().enabled = false;
         dogToy____OHBOYOHBOYOHBOY = false;
         whereMaLooking = false;
+        meAnimationIsRunnin = false;
     }
 
     // Update is called once per frame
@@ -24,10 +30,24 @@ public class DoggoFollow : MonoBehaviour
         {
             dogToy____OHBOYOHBOYOHBOY = false;
             GetComponent<EnemyFollow>().enabled = false;
-        }else if (!dogToy____OHBOYOHBOYOHBOY && GameManager.instance.DogToyOut)
+        }
+        else if (!dogToy____OHBOYOHBOYOHBOY && GameManager.instance.DogToyOut)
         {
             dogToy____OHBOYOHBOYOHBOY = true;
             GetComponent<EnemyFollow>().enabled = true;
+        }
+
+        if (!meAnimationIsRunnin && GetComponent<EnemyFollow>().IsWalking)
+        {
+            Debug.Log("HE BE Runnin");
+            meAnimationIsRunnin = true;
+            GetComponent<Animator>().SetBool("IsWalking", true);
+        }
+        else if (meAnimationIsRunnin && !GetComponent<EnemyFollow>().IsWalking)
+        {
+            Debug.Log("He be Standing");
+            meAnimationIsRunnin = false;
+            GetComponent<Animator>().SetBool("IsWalking", false);
         }
 
         if (dogToy____OHBOYOHBOYOHBOY && Vector2.Distance(transform.position, GiveMeMyOwner.transform.position) < 2)
