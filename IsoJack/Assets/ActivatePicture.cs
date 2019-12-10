e48aeedd23d60ce7af66d1f203fcc1a93023c23d
Aaron Laptop <Aaron1997ps@gmail.com>
2019-12-10 15:51:56 -0600

    PictureEasterEgg

---
+++ IsoJack/Assets/ActivatePicture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePicture : MonoBehaviour
+{
+    public GameObject objectToTrack;
+    public GameObject pictureOverlay;
+
+    private bool pictureActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pictureActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            pictureActive = false;
+            pictureOverlay.SetActive(false);
+        }
+        else if (!pictureActive && Input.GetKeyDown(KeyCode.Space) && Vector2.Distance(objectToTrack.transform.position, transform.position) < 5)
+        {
+            pictureActive = true;
+            pictureOverlay.SetActive(true);
+        }
+    }
+}
